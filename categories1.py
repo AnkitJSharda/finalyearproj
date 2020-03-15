27c2a5b76e8107c55869027edff1e0c84d7875bc
Ankit Sharda <ankit.sharda@CDSYS.LOCAL>
2020-03-15 13:40:36 +0530

    third commit

--- categories1.py
+++ categories1.py
@@ -1,4 +1,5 @@
 import json
+import re
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOptions
@@ -7,14 +8,27 @@ authenticator = IAMAuthenticator('syvCtnQCZoxqdJBqWRBFQQ-6jny7hUUOA8f9WQZdjEGH')
 service = NaturalLanguageUnderstandingV1(version='2018-03-16',authenticator=authenticator)
 service.set_service_url('https://gateway.watsonplatform.net/natural-language-understanding/api')
 
-fp = open('C:/Users/ankit.sharda/Desktop/testfile.txt', 'r')
 
-for _, line in enumerate(fp):
+with open("C:/Users/ankit.sharda/Desktop/FinalyearProject/testfile.txt", "r") as f:
+    content_list = f.read()
+
+op = content_list.split(".")
+op.pop()
+
+lin = []
+for i in op:
+    li = re.sub(r" ?\([^)]+\)", "", i)
+    l = re.sub(r'\[.*\]', '', li)
+    lin.append(l)
+
+for i in lin:
+    print(i)
     response = service.analyze(
-        text = line,
+        text = i,
         features=Features(categories=CategoriesOptions(limit=3))).get_result()
 
     print(json.dumps(response['categories'][0], indent = 2))
 
-fp.close()
-    
+f.close()
+
+
